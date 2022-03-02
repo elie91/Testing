d8c624b183a46ad1210213fac7c87c887a32f6c6
Bismuth <eliebismuth91@gmail.com>
2022-03-02 21:08:29 +0100

    TDD Javascript Jest

---
+++ JavascriptTDD/test/stack.test.js
@@ -0,0 +1,37 @@
+const { Stack } = require("../Stack");
+
+
+describe('My Stack', () => {
+
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  })
+
+  it('is created empty', () => {
+    //toBe check the reference
+    expect(stack.top).toBe(-1);
+    // toEqual check value equality
+    expect(stack.items).toEqual({});
+  });
+
+  it('can push to the top', () => {
+    stack.push('1');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('1');
+
+    stack.push('2');
+    expect(stack.top).toBe(1);
+    expect(stack.peek).toBe('2');
+  });
+
+  it('can pop off', () => {
+    stack.push('1');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('1');
+    stack.pop();
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({})
+  })
+})
